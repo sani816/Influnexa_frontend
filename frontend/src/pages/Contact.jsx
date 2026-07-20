837eb2773e9b16d20f7de99faf7d058f4bb5dda0
sani816 <saniakhatun622@gmail.com>
2026-07-20 17:16:36 +0530

    update

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -230,7 +230,7 @@ console.log("Booking Data:", bookingData);
       {/* Contact Form + Booking */}
       <section className="py-20">
 
-        <div className="max-w-7xl mx-auto px-6">
+        <div className="max-w-15xl mx-auto px-6">
 
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
 
